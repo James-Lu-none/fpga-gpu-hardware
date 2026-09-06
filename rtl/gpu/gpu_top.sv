7daa273484a8312874a7aa85e325c240a71066d0
James-Lu-none <jamesabcde277@gmail.com>
2026-09-06 10:18:50 +0000

    refactor: replace old axi modules with new designs

--- rtl/gpu/gpu_top.sv
+++ rtl/gpu/gpu_top.sv
@@ -2,8 +2,7 @@
 // GPU Top Module
 // Encapsulates the RISC-V Command Processor (CP) and the Graphics Processing Cluster
 
-`include "../common/address.vh"
-
+import address_pkg::*;
 import gpu_pkg::*;
 
 module gpu_top (
@@ -22,7 +21,12 @@ module gpu_top (
     
     // UART Physical Interface
     input wire uart_rxd,
-    output wire uart_txd
+    output wire uart_txd,
+
+    // RISC-V Debug Status
+    output wire rv_reset_n,
+    output wire rv_trap,
+    output wire rv_irq
 );
 
     // Reset Synchronizer to resolve high fanout / recovery time violations
@@ -48,31 +52,21 @@ module gpu_top (
     // 1. RISC-V Command Processor SoC (Control Plane)
     
     // Host Control Registers (Sniffed from AXI Lite Write Channel)
-    reg irq_reg;
-    reg cpu_soft_rst_n; // CPU Soft Reset Control Register
-    always @(posedge clk or negedge sys_rst_n) begin
-        if (!sys_rst_n) begin
-            irq_reg <= 1'b0;
-            cpu_soft_rst_n <= 1'b0; // Default to Reset=0 so CPU waits for firmware load on boot
-        end else begin
-            // only takes lower 17 bits of address to ignore higher bits of PCIE BAR hardware address
-            irq_reg <= s_axi_lite.awvalid && s_axi_lite.wvalid && s_axi_lite.awready && ((s_axi_lite.awaddr & 32'h1FFFF) == `BRAM_IRQ_BASE);
-            
-            // CPU Soft Reset
-            if (s_axi_lite.awvalid && s_axi_lite.wvalid && s_axi_lite.awready && ((s_axi_lite.awaddr & 32'h1FFFF) == `BRAM_CPU_RESET_BASE)) begin
-                cpu_soft_rst_n <= s_axi_lite.wdata[0];
-            end
-        end
-    end
-    
+    wire irq_reg;
+    wire cpu_soft_rst_n; // CPU Soft Reset Control Register
+
     wire internal_cp_trap;
+    
+    assign rv_reset_n = cpu_soft_rst_n;
+    assign rv_trap = internal_cp_trap;
+    assign rv_irq = irq_reg;
     
     picorv32_axi #(
         .ENABLE_IRQ(1),
         .ENABLE_IRQ_QREGS(1),
-        .PROGADDR_RESET(`BRAM_PROGADDR_RESET),
-        .PROGADDR_IRQ(`BRAM_PROGADDR_IRQ),
-        .STACKADDR(`BRAM_STACKADDR)
+        .PROGADDR_RESET(BRAM_PROGADDR_RESET),
+        .PROGADDR_IRQ(BRAM_PROGADDR_IRQ),
+        .STACKADDR(BRAM_STACKADDR)
     ) u_picorv32 (
         .clk(clk),
         .resetn(sys_rst_n & cpu_soft_rst_n),
@@ -113,53 +107,56 @@ module gpu_top (
         .eoi()
     );
 
-    // AXI Crossbar (RISC-V -> BRAM, GPU, UART)
-    axi_crossbar_1to3_0 u_crossbar (
+    axi_lite_if ctrl_axi();
+
+    // AXI Crossbar 2x4 (XDMA, RISC-V -> BRAM, CTRL, GPU, UART)
+    axi_crossbar_1 u_crossbar (
         .aclk(clk),
         .aresetn(sys_rst_n),
         
-        // Slave 0 (From RISC-V)
-        .s_axi_awaddr  (rv_axi.awaddr),
-        .s_axi_awprot  (rv_axi.awprot),
-        .s_axi_awvalid (rv_axi.awvalid),
-        .s_axi_awready (rv_axi.awready),
-        .s_axi_wdata   (rv_axi.wdata),
-        .s_axi_wstrb   (rv_axi.wstrb),
-        .s_axi_wvalid  (rv_axi.wvalid),
-        .s_axi_wready  (rv_axi.wready),
-        .s_axi_bresp   (rv_axi.bresp),
-        .s_axi_bvalid  (rv_axi.bvalid),
-        .s_axi_bready  (rv_axi.bready),
-        .s_axi_araddr  (rv_axi.araddr),
-        .s_axi_arprot  (rv_axi.arprot),
-        .s_axi_arvalid (rv_axi.arvalid),
-        .s_axi_arready (rv_axi.arready),
-        .s_axi_rdata   (rv_axi.rdata),
-        .s_axi_rresp   (rv_axi.rresp),
-        .s_axi_rvalid  (rv_axi.rvalid),
-        .s_axi_rready  (rv_axi.rready),
+        // Slave 0 (From XDMA Host)
+        .s_axi_awaddr  ({s_axi_lite.awaddr, rv_axi.awaddr}),
+        .s_axi_awprot  ({s_axi_lite.awprot, rv_axi.awprot}),
+        .s_axi_awvalid ({s_axi_lite.awvalid, rv_axi.awvalid}),
+        .s_axi_awready ({s_axi_lite.awready, rv_axi.awready}),
+        .s_axi_wdata   ({s_axi_lite.wdata,   rv_axi.wdata}),
+        .s_axi_wstrb   ({s_axi_lite.wstrb,   rv_axi.wstrb}),
+        .s_axi_wvalid  ({s_axi_lite.wvalid,  rv_axi.wvalid}),
+        .s_axi_wready  ({s_axi_lite.wready,  rv_axi.wready}),
+        .s_axi_bresp   ({s_axi_lite.bresp,   rv_axi.bresp}),
+        .s_axi_bvalid  ({s_axi_lite.bvalid,  rv_axi.bvalid}),
+        .s_axi_bready  ({s_axi_lite.bready,  rv_axi.bready}),
+        .s_axi_araddr  ({s_axi_lite.araddr,  rv_axi.araddr}),
+        .s_axi_arprot  ({s_axi_lite.arprot,  rv_axi.arprot}),
+        .s_axi_arvalid ({s_axi_lite.arvalid, rv_axi.arvalid}),
+        .s_axi_arready ({s_axi_lite.arready, rv_axi.arready}),
+        .s_axi_rdata   ({s_axi_lite.rdata,   rv_axi.rdata}),
+        .s_axi_rresp   ({s_axi_lite.rresp,   rv_axi.rresp}),
+        .s_axi_rvalid  ({s_axi_lite.rvalid,  rv_axi.rvalid}),
+        .s_axi_rready  ({s_axi_lite.rready,  rv_axi.rready}),
         
-        // Master Ports (M02: UART, M01: GPU, M00: BRAM)
-        .m_axi_awaddr  ({rv_uart_axil.awaddr, rv_gpu_axil.awaddr, bram_axi.awaddr}),
-        .m_axi_awprot  ({rv_uart_axil.awprot, rv_gpu_axil.awprot, bram_axi.awprot}),
-        .m_axi_awvalid ({rv_uart_axil.awvalid, rv_gpu_axil.awvalid, bram_axi.awvalid}),
-        .m_axi_awready ({rv_uart_axil.awready, rv_gpu_axil.awready, bram_axi.awready}),
-        .m_axi_wdata   ({rv_uart_axil.wdata,   rv_gpu_axil.wdata,   bram_axi.wdata}),
-        .m_axi_wstrb   ({rv_uart_axil.wstrb,   rv_gpu_axil.wstrb,   bram_axi.wstrb}),
-        .m_axi_wvalid  ({rv_uart_axil.wvalid,  rv_gpu_axil.wvalid,  bram_axi.wvalid}),
-        .m_axi_wready  ({rv_uart_axil.wready,  rv_gpu_axil.wready,  bram_axi.wready}),
-        .m_axi_bresp   ({rv_uart_axil.bresp,   rv_gpu_axil.bresp,   bram_axi.bresp}),
-        .m_axi_bvalid  ({rv_uart_axil.bvalid,  rv_gpu_axil.bvalid,  bram_axi.bvalid}),
-        .m_axi_bready  ({rv_uart_axil.bready,  rv_gpu_axil.bready,  bram_axi.bready}),
-        .m_axi_araddr  ({rv_uart_axil.araddr,  rv_gpu_axil.araddr,  bram_axi.araddr}),
-        .m_axi_arprot  ({rv_uart_axil.arprot,  rv_gpu_axil.arprot,  bram_axi.arprot}),
-        .m_axi_arvalid ({rv_uart_axil.arvalid, rv_gpu_axil.arvalid, bram_axi.arvalid}),
-        .m_axi_arready ({rv_uart_axil.arready, rv_gpu_axil.arready, bram_axi.arready}),
-        .m_axi_rdata   ({rv_uart_axil.rdata,   rv_gpu_axil.rdata,   bram_axi.rdata}),
-        .m_axi_rresp   ({rv_uart_axil.rresp,   rv_gpu_axil.rresp,   bram_axi.rresp}),
-        .m_axi_rvalid  ({rv_uart_axil.rvalid,  rv_gpu_axil.rvalid,  bram_axi.rvalid}),
-        .m_axi_rready  ({rv_uart_axil.rready,  rv_gpu_axil.rready,  bram_axi.rready})
+        // Master Ports (M03: UART, M02: GPU, M01: CTRL, M00: BRAM)
+        .m_axi_awaddr  ({rv_uart_axil.awaddr, rv_gpu_axil.awaddr, ctrl_axi.awaddr, bram_axi.awaddr}),
+        .m_axi_awprot  ({rv_uart_axil.awprot, rv_gpu_axil.awprot, ctrl_axi.awprot, bram_axi.awprot}),
+        .m_axi_awvalid ({rv_uart_axil.awvalid, rv_gpu_axil.awvalid, ctrl_axi.awvalid, bram_axi.awvalid}),
+        .m_axi_awready ({rv_uart_axil.awready, rv_gpu_axil.awready, ctrl_axi.awready, bram_axi.awready}),
+        .m_axi_wdata   ({rv_uart_axil.wdata,   rv_gpu_axil.wdata,   ctrl_axi.wdata,   bram_axi.wdata}),
+        .m_axi_wstrb   ({rv_uart_axil.wstrb,   rv_gpu_axil.wstrb,   ctrl_axi.wstrb,   bram_axi.wstrb}),
+        .m_axi_wvalid  ({rv_uart_axil.wvalid,  rv_gpu_axil.wvalid,  ctrl_axi.wvalid,  bram_axi.wvalid}),
+        .m_axi_wready  ({rv_uart_axil.wready,  rv_gpu_axil.wready,  ctrl_axi.wready,  bram_axi.wready}),
+        .m_axi_bresp   ({rv_uart_axil.bresp,   rv_gpu_axil.bresp,   ctrl_axi.bresp,   bram_axi.bresp}),
+        .m_axi_bvalid  ({rv_uart_axil.bvalid,  rv_gpu_axil.bvalid,  ctrl_axi.bvalid,  bram_axi.bvalid}),
+        .m_axi_bready  ({rv_uart_axil.bready,  rv_gpu_axil.bready,  ctrl_axi.bready,  bram_axi.bready}),
+        .m_axi_araddr  ({rv_uart_axil.araddr,  rv_gpu_axil.araddr,  ctrl_axi.araddr,  bram_axi.araddr}),
+        .m_axi_arprot  ({rv_uart_axil.arprot,  rv_gpu_axil.arprot,  ctrl_axi.arprot,  bram_axi.arprot}),
+        .m_axi_arvalid ({rv_uart_axil.arvalid, rv_gpu_axil.arvalid, ctrl_axi.arvalid, bram_axi.arvalid}),
+        .m_axi_arready ({rv_uart_axil.arready, rv_gpu_axil.arready, ctrl_axi.arready, bram_axi.arready}),
+        .m_axi_rdata   ({rv_uart_axil.rdata,   rv_gpu_axil.rdata,   ctrl_axi.rdata,   bram_axi.rdata}),
+        .m_axi_rresp   ({rv_uart_axil.rresp,   rv_gpu_axil.rresp,   ctrl_axi.rresp,   bram_axi.rresp}),
+        .m_axi_rvalid  ({rv_uart_axil.rvalid,  rv_gpu_axil.rvalid,  ctrl_axi.rvalid,  bram_axi.rvalid}),
+        .m_axi_rready  ({rv_uart_axil.rready,  rv_gpu_axil.rready,  ctrl_axi.rready,  bram_axi.rready})
     );
+
 
     // AXI UART Lite
     axi_uartlite_0 u_uart (
@@ -187,19 +184,39 @@ module gpu_top (
         .tx            (uart_txd)
     );
 
+    // AXI Control Registers (Port M01)
+    rv_control_register_0 u_ctrl_regs (
+        .s00_axi_aclk    (clk),
+        .s00_axi_aresetn (sys_rst_n),
+        .irq_out         (irq_reg),
+        .cpu_soft_rst_n_out (cpu_soft_rst_n),
+        .s00_axi_awaddr  (ctrl_axi.awaddr[5:0]),
+        .s00_axi_awprot  (ctrl_axi.awprot),
+        .s00_axi_awvalid (ctrl_axi.awvalid),
+        .s00_axi_awready (ctrl_axi.awready),
+        .s00_axi_wdata   (ctrl_axi.wdata),
+        .s00_axi_wstrb   (ctrl_axi.wstrb),
+        .s00_axi_wvalid  (ctrl_axi.wvalid),
+        .s00_axi_wready  (ctrl_axi.wready),
+        .s00_axi_bresp   (ctrl_axi.bresp),
+        .s00_axi_bvalid  (ctrl_axi.bvalid),
+        .s00_axi_bready  (ctrl_axi.bready),
+        .s00_axi_araddr  (ctrl_axi.araddr[5:0]),
+        .s00_axi_arprot  (ctrl_axi.arprot),
+        .s00_axi_arvalid (ctrl_axi.arvalid),
+        .s00_axi_arready (ctrl_axi.arready),
+        .s00_axi_rdata   (ctrl_axi.rdata),
+        .s00_axi_rresp   (ctrl_axi.rresp),
+        .s00_axi_rvalid  (ctrl_axi.rvalid),
+        .s00_axi_rready  (ctrl_axi.rready)
+    );
+
+
     wire [16:0] brama_addr;
     wire brama_clk, brama_en;
     wire [3:0] brama_we;
     wire [31:0] brama_din, brama_dout;
     wire brama_rst;
-
-    wire [16:0] bramb_addr;
-    wire bramb_clk, bramb_en;
-    wire [3:0] bramb_we;
-    wire [31:0] bramb_din, bramb_dout;
-    wire bramb_rst;
-
-    // all axi controllers to BRAM only takes lower 17 bits of address to ignore higher bits of PCIE BAR hardware address
 
     // AXI BRAM Controller (Port A: RISC-V)
     axi_bram_ctrl_0 u_bram_ctrl_rv (
@@ -234,40 +251,7 @@ module gpu_top (
         .bram_rddata_a(brama_dout)
     );
 
-    // AXI BRAM Controller (Port B: Host PCIe)
-    axi_bram_ctrl_0 u_bram_ctrl_host (
-        .s_axi_aclk(clk),
-        .s_axi_aresetn(sys_rst_n),
-        .s_axi_awaddr(s_axi_lite.awaddr[16:0]),
-        .s_axi_awprot(s_axi_lite.awprot),
-        .s_axi_awvalid(s_axi_lite.awvalid),
-        .s_axi_awready(s_axi_lite.awready),
-        .s_axi_wdata(s_axi_lite.wdata),
-        .s_axi_wstrb(s_axi_lite.wstrb),
-        .s_axi_wvalid(s_axi_lite.wvalid),
-        .s_axi_wready(s_axi_lite.wready),
-        .s_axi_bresp(s_axi_lite.bresp),
-        .s_axi_bvalid(s_axi_lite.bvalid),
-        .s_axi_bready(s_axi_lite.bready),
-        .s_axi_araddr(s_axi_lite.araddr[16:0]),
-        .s_axi_arprot(s_axi_lite.arprot),
-        .s_axi_arvalid(s_axi_lite.arvalid),
-        .s_axi_arready(s_axi_lite.arready),
-        .s_axi_rdata(s_axi_lite.rdata),
-        .s_axi_rresp(s_axi_lite.rresp),
-        .s_axi_rvalid(s_axi_lite.rvalid),
-        .s_axi_rready(s_axi_lite.rready),
-
-        .bram_rst_a(bramb_rst),
-        .bram_clk_a(bramb_clk),
-        .bram_en_a(bramb_en),
-        .bram_we_a(bramb_we),
-        .bram_addr_a(bramb_addr),
-        .bram_wrdata_a(bramb_din),
-        .bram_rddata_a(bramb_dout)
-    );
-
-    // 128KB True Dual-Port Block Memory Generator
+    // 128KB Single-Port Block Memory Generator
     blk_mem_gen_0 u_bram (
         .clka(brama_clk),
         .rsta(brama_rst),
@@ -276,16 +260,7 @@ module gpu_top (
         .addra(brama_addr), // since byte write is enabled so we dont need to do 2 bit shift
         .dina(brama_din),
         .douta(brama_dout),
-        .rsta_busy(),
-
-        .clkb(bramb_clk),
-        .rstb(bramb_rst),
-        .enb(bramb_en),
-        .web(bramb_we),
-        .addrb(bramb_addr), // since byte write is enabled so we dont need to do 2 bit shift
-        .dinb(bramb_din),
-        .doutb(bramb_dout),
-        .rstb_busy()
+        .rsta_busy()
     );
 
     // Interrupt Handshake Logic for PCIe XDMA
