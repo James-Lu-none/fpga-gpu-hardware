c33f5401e63f4d90856e5d962377fab0ed6f872a
James-Lu-none <jamesabcde277@gmail.com>
2026-09-17 05:29:08 +0000

    feat: bump HW_VERSION_MAJOR to 2 in gpu_pkg

--- rtl/common/gpu_pkg.sv
+++ rtl/common/gpu_pkg.sv
@@ -4,7 +4,7 @@
 
 package gpu_pkg;
     // Architecture Version Parameters
-    parameter HW_VERSION_MAJOR = 32'd1;
+    parameter HW_VERSION_MAJOR = 32'd2;
     parameter HW_VERSION_MINOR = 32'd0;
 
     // Architecture Hierarchy Parameters (N, M, K)
